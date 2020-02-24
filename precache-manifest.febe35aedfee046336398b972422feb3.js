92c54c73814e83ca769cdb621a028a0a47666f70
lemonec <pierre.lemonec@gmail.com>
2020-02-25 00:48:41 +0200

    Updates

---
+++ precache-manifest.febe35aedfee046336398b972422feb3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0f9bf156fdefdb8a402fa9d149755e3",
+    "url": "/connectfourtask/index.html"
+  },
+  {
+    "revision": "d1d06e0bff76f42e7793",
+    "url": "/connectfourtask/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "47d4af77f3e87deec424",
+    "url": "/connectfourtask/static/js/2.84e11d27.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/connectfourtask/static/js/2.84e11d27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1d06e0bff76f42e7793",
+    "url": "/connectfourtask/static/js/main.f6e3873e.chunk.js"
+  },
+  {
+    "revision": "1414d6008a71a5bbb1d8",
+    "url": "/connectfourtask/static/js/runtime-main.1838f3aa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/connectfourtask/static/media/logo.5d5d9eef.svg"
+  }
+]);
